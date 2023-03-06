2a2f54e0394fcc1077d5ff8318c522f3afdae5dd
dina-shoham <dina.shoham@gmail.com>
2023-03-06 18:56:10 -0500

    first commit, basic counter functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 function App() {
+  const [patronsIn, setPatronsIn] = useState(0);
+  const [patronsOut, setPatronsOut] = useState(0);
+  const [patronsInPark, setPatronsInPark] = useState(0);
+
+  useEffect(() => {
+    setPatronsInPark(patronsIn - patronsOut);
+
+  }, 
+  [patronsIn, patronsOut]);
+
+  let incrementPatronsIn = () => {
+    setPatronsIn(patronsIn + 1);
+  };
+
+  let incrementPatronsOut = () => {
+    setPatronsOut(patronsOut + 1);
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>OAPatron Counter</h3>
+      <div>
+        <ButtonGroup variant="contained"> 
+          <Button onClick={incrementPatronsOut}>-</Button>
+          <Button>{patronsInPark}</Button>
+          <Button onClick={incrementPatronsIn}>+</Button> 
+        </ButtonGroup>
+      </div>
+      <p>Total patrons in: {patronsIn}</p>
+      <p>Total patrons out: {patronsOut}</p>
     </div>
   );
 }
